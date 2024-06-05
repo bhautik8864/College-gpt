c30abe807e8d8532d3e8c89ee5bdaba7018e31be
Bhautik Khandhala <138150160+bhautik8864@users.noreply.github.com>
2024-06-05 12:06:04 +0530

    Add files via upload

---
+++ gpt.js
@@ -0,0 +1,8 @@
+//function downloadPDF(){
+  //  const pdfURL="C:\Users\bhaut\OneDrive\Desktop\link dig.pdf";
+    //window.open(pdfURL,"_blank");
+
+//}
+document.querySelector('.downlod-button').addEventListener('click',function(){
+
+});
